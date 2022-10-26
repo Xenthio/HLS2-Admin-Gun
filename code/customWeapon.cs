bd70daaedc8bd1b3278909f02e431a9910cd99f5
Xenthio <thegooster88@gmail.com>
2022-10-27 09:44:06 +1100

    gamer

---
+++ code/customWeapon.cs
@@ -0,0 +1,24 @@
+﻿using Sandbox;
+class CoolGun : HLWeapon
+{
+    public override string InventoryIcon => "/ui/admingun.png";
+    public override string InventoryIconSelected => "/ui/admingun.png";
+    public override float PrimaryRate => 0.001f;
+    public override int Bucket => 1;
+    public override int BucketWeight => 3;
+    public override void AttackPrimary()
+    {
+        base.AttackPrimary();
+        if ( IsClient ) return;
+        ShootBullet( 0f, 100, 100, 1 );
+    }
+    public override void AttackSecondary()
+    {
+        base.AttackSecondary();
+        if ( IsClient ) return;
+        var tr = Trace.Ray( Owner.EyePosition, Owner.EyePosition + Owner.EyeRotation.Forward * 10000 ).Ignore(Owner).Run();
+        var ex = new env_explosion();
+        ex.Position = tr.EndPosition;
+        ex.Explode();
+    }
+}
