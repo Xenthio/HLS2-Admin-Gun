f1b901040481979842d4a7508347fa2b65f87dc4
Xenthio <thegooster88@gmail.com>
2022-10-27 17:08:08 +1100

    Update customWeapon.cs

--- code/customWeapon.cs
+++ code/customWeapon.cs
@@ -4,6 +4,7 @@ class CoolGun : Weapon
     public override string InventoryIcon => "/ui/admingun.png";
     public override string InventoryIconSelected => "/ui/admingun.png";
     public override float PrimaryRate => 0.001f;
+    public override float ReloadTime => 0.01f;
     public override int Bucket => 1;
     public override int BucketWeight => 3;
     public override void AttackPrimary()
@@ -17,8 +18,6 @@ class CoolGun : Weapon
         base.AttackSecondary();
         if ( IsClient ) return;
         var tr = Trace.Ray( Owner.EyePosition, Owner.EyePosition + Owner.EyeRotation.Forward * 10000 ).Ignore(Owner).Run();
-        var ex = new env_explosion();
-        ex.Position = tr.EndPosition;
-        ex.Explode();
+        HLExplosion.Explosion( this, Owner, tr.EndPosition, 250, 100, 24.0f, "grenade" );
     }
 }
